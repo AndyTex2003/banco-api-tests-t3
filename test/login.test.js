6e1e84c3a7e3b8f16c81142c03c84971441d4fe4
tex batista <andersonrexbatista@gmail.com>
2026-02-26 16:47:22 -0300

    Implementando o Mochawesome para geração de relatório em HTML

--- test/login.test.js
+++ test/login.test.js
@@ -10,11 +10,11 @@ describe('Login', () => {
                 .send({
                     'username': 'julio.lima',
                     'senha': '123456'
-                  })           
+                  });           
             
             expect(resposta.status).to.equal(200);
             expect(resposta.body.token).to.be.a('string');        
-        })
+        });
 
-    })
-})
+    });
+});
