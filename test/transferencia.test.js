d6eec03a0b1fd3fb390e658b8924093b2c0c4517
tex batista <andersonrexbatista@gmail.com>
2026-03-09 15:03:35 -0300

    Adicionando a biblioteca .env para variáveis globais

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 
 describe('Transferencias', () => {
     describe('POST /transferencias', () => {
-        it('Deve retornar sucesso com 201 quando o valor da transferencia for igual ou acima de R$ 10,00', async () => {                  
+        it('Deve retornar sucesso com 201 quando o valor da transferencia for igual ou acima de R$ 10,00', async () => {          
             const respostaLogin = await request(process.env.BASE_URL)
                 .post('/login')
                 .set('Content-Type', 'application/json')
@@ -40,7 +40,7 @@ describe('Transferencias', () => {
             
             const token = respostaLogin.body.token   
 
-            const resposta = await request('http://localhost:3000')
+            const resposta = await request(process.env.BASE_URL)
                .post('/transferencias')
                .set('Content-Type', 'application/json')
                .set('Authorization', `Bearer ${token}`)
@@ -55,4 +55,4 @@ describe('Transferencias', () => {
 
         })
     })
-})
+}) 
